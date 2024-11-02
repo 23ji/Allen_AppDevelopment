77810aef838ef0d2f2197a80850b1d635797112e
23ji <isj4194@naver.com>
2024-11-02 17:26:36 +0900

    Initial Commit

---
+++ FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by 이상지 on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
