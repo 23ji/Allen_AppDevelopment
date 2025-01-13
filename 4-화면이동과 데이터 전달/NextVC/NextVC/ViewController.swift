b59728e8597d898ec98a3efc85d5269515c8cdde
23ji <isj4194@naver.com>
2025-01-13 21:08:51 +0900

    스토리보드 with Segue 화면 전환 방법

--- 4-화면이동과 데이터 전달/NextVC/NextVC/ViewController.swift
+++ 4-화면이동과 데이터 전달/NextVC/NextVC/ViewController.swift
@@ -25,9 +25,15 @@ class ViewController: UIViewController {
     
     // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
+        // 🍑 (애플이 만들어 놓은) 구체적이지 않은 타입이므로 타입 캐스팅을 통해 구체적으로
+        
+        if let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController{
+            secondVC.someString = "반가워요"
 
-        
-        
+            present(secondVC, animated: true, completion: nil)
+            
+            
+        }
         
         
     }
@@ -37,13 +43,19 @@ class ViewController: UIViewController {
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
         
-
-        
-        
-        
+        // 🍑 sender(보내는 것) = self = ViewController
+        performSegue(withIdentifier: "toThirdVC", sender: self)
         
     }
     
+    // 🍑 데이터를 전달받으려면 어떤 메서드를 재정의해서 구현해줘야함 (위에처럼 일반적인 someString 불가)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toThirdVC"{
+            let thirdVC = segue.destination as! ThirdViewController
+            // 데이터 전달
+            thirdVC.someString = "세번째 화면입니다."
+        }
+    }
 
     
 }
