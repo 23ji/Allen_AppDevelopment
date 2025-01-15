05e4d5852dc4814976dceaa820d840e7c8e2e955
23ji <isj4194@naver.com>
2025-01-15 17:24:26 +0900

    레이블과 버튼 코드 연결

--- 4-화면이동과 데이터 전달/BMI/BMI/ViewController.swift
+++ 4-화면이동과 데이터 전달/BMI/BMI/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLable: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTExtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
