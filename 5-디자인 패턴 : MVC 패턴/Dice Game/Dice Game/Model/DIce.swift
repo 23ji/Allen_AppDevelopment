675b72535f0e105c625941e3fcc956c56a648a64
23ji <isj4194@naver.com>
2025-02-11 08:16:35 +0900

    UI 설정 및 Assets 추가

---
+++ 5-디자인 패턴 : MVC 패턴/Dice Game/Dice Game/Model/DIce.swift
@@ -0,0 +1,13 @@
+//
+//  DIce.swift
+//  Dice Game
+//
+//  Created by 이상지 on 2/11/25.
+//
+
+import Foundation
+import UIKit
+
+struct DiceImage{
+    var diceImage: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+}
