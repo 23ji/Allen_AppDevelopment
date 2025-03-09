916c88a601a25fa497dba081e79aea864340bf06
23ji <isj4194@naver.com>
2025-03-09 18:46:40 +0900

    테이블뷰 실습

---
+++ 7-테이블뷰+화면이동/TableView-23G/TableView-23G/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView-23G
+//
+//  Created by 이상지 on 3/9/25.
+//
+
+import UIKit
+
+struct Movie {
+    var mainImageView: UIImage?
+    let movieNameLabel: String
+    let movieDescriptionLable: String
+}
