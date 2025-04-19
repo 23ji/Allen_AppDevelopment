b37b551632b92cbd63ce6b230e758e66524a1b9e
23ji <isj4194@naver.com>
2025-04-19 17:34:21 +0900

    처음 상태로 되돌리기

--- 7-테이블뷰+화면이동/TableView-23G/TableView-23G/Controller/ViewController.swift
+++ 7-테이블뷰+화면이동/TableView-23G/TableView-23G/Controller/ViewController.swift
@@ -2,42 +2,17 @@
 //  ViewController.swift
 //  TableView-23G
 //
-//  Created by 이상지 on 3/9/25.
+//  Created by 이상지 on 4/19/25.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-    
-    var movieArray: [Movie] = [
-        Movie(mainImageView: UIImage(named: "batman.png"), movieNameLabel: "베트맨", movieDescriptionLable: "베트맨이 나오는 영화"),
-        Movie(mainImageView: UIImage(named: "hulk.png"), movieNameLabel: "헐크", movieDescriptionLable: "헐크가 나오는 영화")
-    ]
+class ViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
-        cell.mainImageView.image = movieArray[indexPath.row].mainImageView
-        cell.movieNameLabel.text = movieArray[indexPath.row].movieNameLabel
-        cell.movieDescriptionLable.text = movieArray[indexPath.row].movieDescriptionLable
-        
-        return cell
-    }
 
+        // Do any additional setup after loading the view.
+    }
 
 }
-
