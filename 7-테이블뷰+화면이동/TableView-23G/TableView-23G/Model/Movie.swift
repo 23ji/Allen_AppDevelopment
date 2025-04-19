4a1f015ac6e4a8420e4fe2c722132acc24c6494a
23ji <isj4194@naver.com>
2025-04-19 17:38:11 +0900

    Movie 모델 생성

---
+++ 7-테이블뷰+화면이동/TableView-23G/TableView-23G/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView-23G
+//
+//  Created by 이상지 on 4/19/25.
+//
+
+import UIKit
+
+struct Movie {
+  let movieImage: UIImage
+  let movieName: String
+  let descriptionLabel: String
+}
