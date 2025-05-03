1c8503cadd87c6a901138a593be5f585952323c5
23ji <isj4194@naver.com>
2025-05-03 21:08:18 +0900

    DetailView UI 구현

---
+++ 8-커스텀 델리게이트 패턴의 활용/MemberList/MemberList/Controller/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  MemberList
+//
+//  Created by 이상지 on 5/3/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
