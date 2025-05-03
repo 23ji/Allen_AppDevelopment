79b1550b0504235074273051bcad638148458415
23ji <isj4194@naver.com>
2025-05-03 19:14:11 +0900

    initial commit

---
+++ 8-커스텀 델리게이트 패턴의 활용/MemberList/MemberList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by 이상지 on 5/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
